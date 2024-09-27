38067b5dd7988f24949314096461764ea3a6475d
Héctor Antonio Gonzalez Leal <hector.gonzalez@omnilife.com>
2024-09-27 14:59:49 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
+import React from 'react';
+import './bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import UsuarioList from './componentes/Usuarios/UsuarioList';
+import EmpleadoList from './componentes/Empleados/EmpleadoList';
+import EstacionamientoList from './componentes/Estacionamientos/EstacionamientoList';
+import ReservaList from './componentes/Reservas/ReservaList';
+import LiberacionList from './componentes/Liberaciones/LiberacionList';
+import LogList from './componentes/Logs/LogList';
+import LoginForm, {} from "./componentes/LoginForm"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginForm></LoginForm>
+       <div className="App">
+        <div className="container">
+          <h1>Sistema de Gestión de Estacionamientos</h1>
+          <UsuarioList />
+          <EmpleadoList />
+          <EstacionamientoList />
+          <ReservaList />
+          <LiberacionList />
+          <LogList />
+        </div>
+      </div>
     </div>
   );
 }
