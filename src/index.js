ee9b864a842b957490fee64d054c2784b57ef804
H3ct0rGL <131729951+H3ct0rGL@users.noreply.github.com>
2024-09-28 20:02:37 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, 
-  document.getElementById("root")
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 /*
 const root = ReactDOM.createRoot(document.getElementById('root'));
